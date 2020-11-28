3a0fcec19efd65d717b7a1b8aaea42128df916a8
HuffDaddy0 <ty.huff24@gmail.com>
2020-11-28 16:48:28 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,18 @@ class App extends Component {
     this.handleAddTimer()
   }
 
+  
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+  
+  componentDidUpdate(){
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
   render() {
     console.log(this.state.timerIDs);
     return (
